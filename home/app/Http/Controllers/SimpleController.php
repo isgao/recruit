62c99471f95d87dea791c94dc6a6572535535e4b
stone <shilei_zhang@163.com>
2016-11-10 09:58:15 +0800

    注册与登录模板整理好了 Signed-off-by: stone <shilei_zhang@163.com>

---
+++ home/app/Http/Controllers/SimpleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SimpleController extends Controller
+{
+
+	/**
+	 * [reg_act 用户注册]
+	 */
+    public function reg_act()
+    {
+    	
+    }
+}
